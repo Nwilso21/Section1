35a1408b342fe2ee27b8a2a9afc496ae3391153b
Nwilso21 <noahwilson6381@gmail.com>
2024-07-31 18:32:57 -0600

    Created add.py
    add.py is a file that takes in two variable values and outputs them

---
+++ 1.1/add.py
@@ -0,0 +1,5 @@
+print("this program will add two numbers together")
+a= int(input("Enter the first number:  "))
+b= int(input("Enter the second number:  "))
+c= a+b
+print("The value of " + str(a) + " added to " + str(b) + " is "+ str(c))
